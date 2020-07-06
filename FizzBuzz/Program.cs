722fba816acecd2d2ff21dc073f0dc76d0abc570
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-06 15:26:41 +0100

    Code Refactoring

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -9,50 +9,68 @@ namespace FizzBuzz
         {
             for (int i = 1; i < 361; i++)
             {
-                //Keep track of what rules/words this number has triggered, and the order the words should be spoken in
-                string[] toPrintList = {"", "", "", "", ""};
-                string toPrint = "";
-
-                if (i % 3 == 0)
-                {
-                    toPrintList[0] = "Fizz";
-                }
-                if (i % 13 == 0)
-                {
-                    toPrintList[1] = "Fezz";
-                }
-                if (i % 5 == 0)
-                {
-                    toPrintList[2] = "Buzz";
-                }
-                if (i % 7 == 0)
-                {
-                    toPrintList[3] = "Bang";
-                }
-                if (i % 11 == 0)
-                {
-                    toPrintList[0] = "";
-                    toPrintList[2] = "";
-                    toPrintList[3] = "Bong";
-                }
-                if (i % 17 == 0)
-                {
-                    Array.Reverse(toPrintList);
-                }
-               
-                foreach (string word in toPrintList)
-                {
-                    toPrint += word;
-                }
-
-                //If the number triggered no rules, print just the number
-                if (toPrint == "")
-                {
-                    toPrint = i.ToString();
-                }
-                
-                Console.WriteLine(toPrint);
+                string phrase = GetPhraseForNumber(i);
+                PrintPhrase(phrase);
             }
+        }
+        
+        static string GetPhraseForNumber(int number)
+        {
+            string phrase = "";
+            string[] wordsToSay = ApplyRulesToNumber(number); 
+            
+            foreach (string word in wordsToSay)
+            {
+                phrase += word;
+            }
+
+            //If the number triggered no rules, return just the number
+            if (phrase == "")
+            {
+                phrase = number.ToString();
+            }
+
+            return phrase;
+        }
+        
+        static string[] ApplyRulesToNumber(int i)
+        {
+            //Keep track of what rules/words this number has triggered, and the order the words should be spoken in
+            string[] wordsToSay = {"", "", "", "", ""};
+            
+            if (i % 3 == 0)
+            {
+                wordsToSay[0] = "Fizz";
+            }
+            if (i % 13 == 0)
+            {
+                wordsToSay[1] = "Fezz";
+            }
+            if (i % 5 == 0)
+            {
+                wordsToSay[2] = "Buzz";
+            }
+            if (i % 7 == 0)
+            {
+                wordsToSay[3] = "Bang";
+            }
+            if (i % 11 == 0)
+            {
+                wordsToSay[0] = "";
+                wordsToSay[2] = "";
+                wordsToSay[3] = "Bong";
+            }
+            if (i % 17 == 0)
+            {
+                Array.Reverse(wordsToSay);
+            }
+
+            return wordsToSay;
+        }
+        
+        static void PrintPhrase(string phrase)
+        {
+            Console.WriteLine(phrase);
         }
     }
 }
