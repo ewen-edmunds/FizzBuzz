7b3da83d06d42f05be4911d0b306be387fb8f8e6
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-06 14:25:38 +0100

    Initial Commit
    Added the first part of the FizzBuzz task:
    The basic fizz and buzz rules, printed to the console

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                //Keep track of what rules/words this number has triggered
+                string toPrint = "";
+
+                if (i % 3 == 0)
+                {
+                    toPrint += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    toPrint += "Buzz";
+                }
+                
+                //If the number triggered no rules, print just the number
+                if (toPrint == "")
+                {
+                    toPrint = i.ToString();
+                }
+                
+                Console.WriteLine(toPrint);
+            }
+        }
+    }
+}
