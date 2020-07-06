2032aa8f14511ba9fd7980de8856f6f97b2467e7
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-06 15:15:44 +0100

    Added Fezz and Reversal Rules
    Ambiguity: How reversal interacts with Fezz.
    This implementation includes both Fezz and Bong in the 'etc.' included in the reversal rule. This allows Fezz to be reversed, meaning it might occur after something beginning with B.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace FizzBuzz
 {
@@ -6,28 +7,44 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < 361; i++)
             {
-                //Keep track of what rules/words this number has triggered
+                //Keep track of what rules/words this number has triggered, and the order the words should be spoken in
+                string[] toPrintList = {"", "", "", "", ""};
                 string toPrint = "";
 
                 if (i % 3 == 0)
                 {
-                    toPrint += "Fizz";
+                    toPrintList[0] = "Fizz";
+                }
+                if (i % 13 == 0)
+                {
+                    toPrintList[1] = "Fezz";
                 }
                 if (i % 5 == 0)
                 {
-                    toPrint += "Buzz";
+                    toPrintList[2] = "Buzz";
                 }
                 if (i % 7 == 0)
                 {
-                    toPrint += "Bang";
+                    toPrintList[3] = "Bang";
                 }
                 if (i % 11 == 0)
                 {
-                    toPrint = "Bong";
+                    toPrintList[0] = "";
+                    toPrintList[2] = "";
+                    toPrintList[3] = "Bong";
                 }
-                
+                if (i % 17 == 0)
+                {
+                    Array.Reverse(toPrintList);
+                }
+               
+                foreach (string word in toPrintList)
+                {
+                    toPrint += word;
+                }
+
                 //If the number triggered no rules, print just the number
                 if (toPrint == "")
                 {
